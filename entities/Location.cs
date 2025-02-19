61bed507403fa91eefb57bfc40f3140411372e9c
DaniJ2005 <Danijurjevic2005@gmail.com>
2025-02-19 12:55:47 +0100

    Create Location.cs
    Created Location Class and defined fiels & constructor

---
+++ entities/Location.cs
@@ -0,0 +1,25 @@
+public class Location
+{
+    // Required properties (set during construction)
+    public int ID { get; }
+    public string Name { get; }
+    public string Description { get; }
+    public Quest QuestAvailableHere { get; }
+    public Monster MonsterLivingHere { get; }
+
+    // Neighboring locations (can be assigned after creation)
+    public Location LocationToNorth { get; set; }
+    public Location LocationToEast { get; set; }
+    public Location LocationToSouth { get; set; }
+    public Location LocationToWest { get; set; }
+
+    // Constructor to initialize required fields
+    public Location(int id, string name, string description, Quest questAvailableHere, Monster monsterLivingHere)
+    {
+        ID = id;
+        Name = name;
+        Description = description;
+        QuestAvailableHere = questAvailableHere;
+        MonsterLivingHere = monsterLivingHere;
+    }
+}
