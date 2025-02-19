6a725924c95dd0c368e46ff5ec00422bffed1224
Peter Zoetekouw <pmjzoetekouw@hotmail.com>
2025-02-19 13:56:22 +0100

    Added Monster class

---
+++ entities/Monster.cs
@@ -0,0 +1,17 @@
+class Monster
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public int MaximumDamage { get; set; }
+    public int CurrentHitPoints { get; set; }
+    public int MaximumHitPoints { get; set; }
+
+    public Monster(int id, string name, int maxDamage, int maxHitPoints) //defaults worden geïnitialiseerd bij het aanmaken van een object van deze class
+    {
+        ID = id;
+        Name = name;
+        MaximumDamage = maxDamage;
+        MaximumHitPoints = maxHitPoints;
+        CurrentHitPoints = maxHitPoints;
+    }
+}    
