c86f68071fb9785f897ab6b7489a1ebc056643aa
hogeschoolzeiler <1083330@hr.nl>
2025-02-20 15:24:26 +0100

    Weapon class aangemaakt

---
+++ entities/Weapon.cs
@@ -0,0 +1,13 @@
+public class Weapon
+{
+    public int ID { get; }
+    public string Name { get; }
+    public int MaximumDamage { get; }
+
+    public Weapon(int id, string name, int maximumdamage)
+    {
+        ID = id;
+        Name = name;
+        MaximumDamage = maximumdamage;
+    }
+}
