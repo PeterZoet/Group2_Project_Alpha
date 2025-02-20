afb0d21095f88316b6ec0c5724696d2e4ccce69c
Elias <bertelias2005@gmail.com>
2025-02-20 14:41:56 +0100

    Add files via upload

---
+++ entities/Player.cs
@@ -0,0 +1,17 @@
+public class Player
+{
+    public string Name { get; set; }
+    public int CurrentHitPoints { get; set; }
+    public int MaximumHitPoints { get; set; }
+    public Weapon CurrentWeapon { get; set; }
+    public Location CurrentLocation { get; set; }
+
+    public Player(string name, int maxHitPoints, Weapon currentWeapon, Location currentLocation)
+    {
+        Name = name;
+        MaximumHitPoints = maxHitPoints;
+        CurrentHitPoints = maxHitPoints;
+        CurrentWeapon = currentWeapon;
+        CurrentLocation = currentLocation;
+    }
+}
