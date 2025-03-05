b80097b27208a9cdb653f71349d19edd36b85994
DaniJ2005 <Danijurjevic2005@gmail.com>
2025-03-05 14:51:02 +0100

    Created DisplayWorldAttribute method
    Created DiisplayWorldAttribute method in Main()
    
    Displays all weapons & locations with their respective quests and monsters

--- Program.cs
+++ Program.cs
@@ -1,9 +1,30 @@
-﻿namespace _2425_OP34_Group2_Project_Alpha;
+﻿using entities;
+namespace _2425_OP34_Group2_Project_Alpha;
 
 class Program
 {
     static void Main()
     {
         Console.WriteLine("Hello, World!");
+
+        DisplayWorldAttributes();
+    }
+
+    public static void DisplayWorldAttributes()
+    {
+        Console.WriteLine("\nAvailable Weapons:");
+        foreach (var weapon in World.Weapons)
+        {
+            Console.WriteLine($"{weapon.ID}: {weapon.Name} (Damage: {weapon.MaximumDamage})");
+        }
+
+        Console.WriteLine("\nAvailable Locations:\n");
+        foreach (var location in World.Locations)
+        {
+            Console.WriteLine($"{location.ID}: {location.Name} - {location.Description}");
+            Console.WriteLine($"   Quest: {(location.QuestAvailableHere != null ? $"{location.QuestAvailableHere.Name} - id {location.QuestAvailableHere.ID}" : "None")}");
+            Console.WriteLine($"   Monster: {(location.MonsterLivingHere != null ? $"{location.MonsterLivingHere.Name} - id {location.MonsterLivingHere.ID}" : "None")}\n");
+        }
+
     }
 }
